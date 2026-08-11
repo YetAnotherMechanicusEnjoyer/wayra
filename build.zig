a29ad77c2768d16db4e67136d5a4e64ed2c4cda0
YetAnotherMechanicusEnjoyer <ahmed.al-rawi@epitech.eu>
2026-08-11 09:31:16 +0200

    feat: update builds

--- build.zig
+++ build.zig
@@ -3,6 +3,7 @@ const std = @import("std");
 pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
+    const run_step = b.step("run", "Run executable");
     const test_step = b.step("test", "Run tests");
     const docs_step = b.step("docs", "Generate documentation");
 
@@ -16,18 +17,18 @@ pub fn build(b: *std.Build) void {
     module.addImport("string", libstring.module("string"));
 
     const exe = b.addExecutable(.{
-        .name = "your-project-name",
+        .name = "wayra",
         .root_module = module,
     });
 
     const tests_mod = b.createModule(.{
-        .root_source_file = b.path("tests/your-project-name.zig"),
+        .root_source_file = b.path("tests/wayra.zig"),
         .target = target,
         .optimize = optimize,
     });
 
     tests_mod.addImport("string", libstring.module("string"));
-    tests_mod.addImport("your-project-name", module);
+    tests_mod.addImport("wayra", module);
 
     const tests = b.addTest(.{
         .name = "string_test",
@@ -43,6 +44,9 @@ pub fn build(b: *std.Build) void {
         .install_subdir = "docs",
     });
     docs_step.dependOn(&install_docs.step);
+
+    const run_exe = b.addRunArtifact(exe);
+    run_step.dependOn(&run_exe.step);
 
     b.installArtifact(exe);
 }

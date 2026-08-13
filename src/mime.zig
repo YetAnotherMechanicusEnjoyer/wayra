985ee0f9fc9a67bbd01dd3f8f1c0304d208b50a1
YetAnotherMechanicusEnjoyer <ahmed.al-rawi@epitech.eu>
2026-08-13 19:00:37 +0200

    fix: added wasm mime type

--- src/mime.zig
+++ src/mime.zig
@@ -7,6 +7,7 @@ pub fn get_mime(path: []const u8) []const u8 {
     if (std.ascii.eqlIgnoreCase(ext, ".css")) return "text/css; charset=utf-8";
     if (std.ascii.eqlIgnoreCase(ext, ".js")) return "application/javascript; charset=utf-8";
     if (std.ascii.eqlIgnoreCase(ext, ".json")) return "application/json; charset=utf-8";
+    if (std.ascii.eqlIgnoreCase(ext, ".wasm")) return "application/wasm";
     if (std.ascii.eqlIgnoreCase(ext, ".png")) return "image/png";
     if (std.ascii.eqlIgnoreCase(ext, ".jpg") or std.ascii.eqlIgnoreCase(ext, ".jpeg")) return "image/jpeg";
     if (std.ascii.eqlIgnoreCase(ext, ".gif")) return "image/gif";
